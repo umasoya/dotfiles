b666fd85943653b744a96a74b10a1ce2a14b2117
umasoya <basilisk.0331@icloud.com>
2015-08-22 08:32:20 +0900

    Add offline pattern

--- bin/get_ssid.sh
+++ bin/get_ssid.sh
@@ -2,4 +2,10 @@
 
 # Get SSID Name
 #networksetup -getairportnetwork en0 | awk '{print $4}'
-networksetup -getairportnetwork en0 | awk '{print $4}'
+SSID=`networksetup -getairportnetwork en0 | awk '{print $4}'`
+#echo 'SSID:'$SSID
+
+if [ $SSID=associated ];then
+	echo OFFLINE
+	exit 0
+fi
