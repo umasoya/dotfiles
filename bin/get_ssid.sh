ea5cb9f3b2b4bad2e3e0a9a7ea4e44db1380132a
umasoya <basilisk.0331@icloud.com>
2015-08-26 09:07:25 +0900

    test ver

--- bin/get_ssid.sh
+++ bin/get_ssid.sh
@@ -5,7 +5,11 @@
 SSID=`networksetup -getairportnetwork en0 | awk '{print $4}'`
 #echo 'SSID:'$SSID
 
-if [ $SSID=associated ];then
+if [[ $SSID=="associated" ]];then
 	echo OFFLINE
 	exit 0
+else
+	echo $SSID
 fi
+
+echo 'test $SSID:'$SSID
