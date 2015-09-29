6507ac12f4b33d6765a97c831f59771406ad5ea4
umasoya <basilisk.0331@icloud.com>
2015-09-30 01:31:57 +0900

    Create symbolic-link to HOME directory

---
+++ dotlink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+## Make Synbolic-link to $HOME
+# ln -s <source> <target>
+
+files="$HOME/dotfiles/.*"
+# Make array
+dotfile=()
+
+# Distinguish file or directory
+for i in $files; do
+	if [ -f $i ]; then
+		dotfile+=("$i")
+	fi
+done
+
+# Make Synbolic-link
+for n in ${dotfile[@]}; do
+	filename=`basename $n`
+	ln -s $n $HOME/$filename
+done
