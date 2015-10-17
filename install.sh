7a0ffccc1734fb2707c5980459b938d5544e1f71
umasoya <basilisk.0331@icloud.com>
2015-10-18 02:12:36 +0900

    Wrote sentence of OSX

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
-# =========================
-#
+# ==============================================================
 ## install.sh
-## Deployment of dotfiles.
+## 1. Deployment of dotfiles.
+## 2. Update shell
+## 3. Change default shell (osx=>newer zsh,linux=>newer bash).
 #
-# How to use it
-# 1. Repository clone to $HOME directory.
-# 	$ git clone https://github.com/yasuto777/dotfiles.git
-# 2. Run install.sh
-# 	$ cd dotfiles && ./install.sh
-# =========================
+#
+## How to use it
+## 1. Repository clone to $HOME directory.
+## 	$ git clone https://github.com/yasuto777/dotfiles.git
+## 2. Run install.sh
+## 	$ cd dotfiles && ./install.sh
+# ==============================================================
 
 # {{{ Detect OS
 detect_os(){
@@ -20,4 +21,33 @@ detect_os(){
 		*'linux'*) PLATFORM='linux'	;;
 	esac
 }
+detect_os
+# }}}
+
+# {{{ OS X
+
+# Symlink to $HOME
+./dotlink.sh
+
+# Install Homebrew if not installed.
+if ! type -p brew > /dev/null ; then
+	echo ""
+	echo "==========================="
+	echo "Installing Homebrew!!"
+	echo "==========================="
+	echo ""
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew doctor
+brew update
+
+echo ""
+echo "Finished Installing & updating Homebrew"
+echo ""
+
+# Update zsh
+# brew install zsh
+
+
+
 # }}}
