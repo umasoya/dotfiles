f492a153aaf3ba447946ddffd73b86526f09ebb8
umasoya <basilisk.0331@icloud.com>
2015-10-30 21:29:34 +0900

    Dock,Terminal,Trackpad

--- mac_setting.sh
+++ mac_setting.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# {{{1 Dock
+
+# Automatically hide or show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# }}}
+
 # {{{1 Finder
 
 	# Show Status-bar in Finder
@@ -17,8 +24,19 @@
 
 # {{{1 Terminal.app
 
-# Use a custom theme （カスタムテーマを使う、そのテーマをデフォルトに設定する）
-# Use a modified version of the Solarized Dark theme by default in Terminal.app
+## Use a custom theme （Use Custom theme, and use it default）
+## Use a modified version of the Solarized Dark theme by default in Terminal.app
 #	TERM_PROFILE='Solarized_Dark';
+## *.terminal path
 #	TERM_PATH='./';
+
+# }}}
+
+# {{{1 TrackPad
+
+# Enable to 'tap to click'
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 # }}}
