effef7209ab33097d81232ad38c3f8c529f75d24
umasoya <basilisk.0331@icloud.com>
2015-11-15 01:22:54 +0900

    Add emoji

--- bin/battery.sh
+++ bin/battery.sh
@@ -2,32 +2,31 @@
 # battery.sh 
 
 # Checking whether AC adapter are connected
-	power_source=`pmset -g ps | awk 'NR==2{print $3}'`
+	is_charging=`pmset -g ps | awk 'NR==2{print $3}'`
 # Get battery level
 	parcentage=`pmset -g ps |awk 'NR==2{print $2}' | awk 'BEGIN{FS="%;";}{print $1}'` 
 
-#------------
-# Rewrite
-#------------
-
 # If AC adapter are connected, color is Yellow
-if [ "${power_source}" = "charging;" ]; then
+if [ "${is_charging}" != "discharging;" ]; then
 	# If charging, font colour is yellow
 	fgcolour="#[fg=colour226]"
+	emoji="\U1F50C"
+else
+	emoji="\U1F50B"
 
-# 100% Blue
-elif [ "${parcentage}" -eq "100" ]; then 
-	fgcolour='#[fg=colour51]'
-
-# 31% ~ 99% Green
-elif [ "${parcentage}" -gt "30" ] && [ "${parcentage}" -lt "100" ]; then
-	fgcolour="#[fg=colour46]"
-
-# 1% ~ 30% Red
-elif [ "${parcentage}" -le "30" ]; then
-	fgcolour="#[fg=colour197]"
+	# 100% Blue
+	if [ "${parcentage}" -eq "100" ]; then 
+		fgcolour='#[fg=colour51]'
+	
+	# 31% ~ 99% Green
+	elif [ "${parcentage}" -gt "30" ] && [ "${parcentage}" -lt "100" ]; then
+		fgcolour="#[fg=colour46]"
+	
+	# 1% ~ 30% Red
+	elif [ "${parcentage}" -le "30" ]; then
+		fgcolour="#[fg=colour197]"
+	fi
 
 fi
 
-echo -e ${fgcolour}${parcentage}'%#[default]'
-
+echo -e ${emoji} ${fgcolour}${parcentage}'%#[default]'
