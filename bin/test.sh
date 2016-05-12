80d57f9bcafca491be3f05406accd4534a953f47
umasoya <basilisk.0331@icloud.com>
2016-05-12 07:30:04 -0700

    test

--- bin/test.sh
+++ bin/test.sh
@@ -1,27 +1,21 @@
-#!/bin/bash
+#!/usr/local/bin/zsh
+
+#case "${OSTYPE}" in
+#	*darwin*)
+#		# OSX
+#		Distribution="OSX"
+#		;;
+#	*linux*)
+#		# Linux
+#		detect_distribution
+#		fi
+#		;;
+#esac
 
 function detect_distribution(){
 	Distribution=`cat /etc/os-release | awk -F'["]' 'NR==1{print $2}' | awk '{print $1}'`
 }
 
-case "${OSTYPE}" in
-	*darwin*)
-		# OSX
-		Distribution="OSX"
-		source "${ZDOT_DIR}/.zshrc_osx"
-		;;
-	*linux*)
-		# Linux
-		detect_distribution
-		if [ -f ${ZDOT_DIR}/.zshrc_${DISTRIBUTION} ]; then
-		source ${ZDOT_DIR}/.zshrc_${DISTRIBUTION}
-		fi
-		;;
-esac
-export Distribution
-
-#function test() {
-#	echo "$distribution"
-#}
-tmux display "DISTRIBUTION: $DISTRIBUTION"
-#tmux display "$1"
+pwd=`pwd`
+tmux display "Distribution: $Distribution"
+tmux display $pwd
