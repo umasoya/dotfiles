c1d0bfce4926268079898ef55f95b131c89ce26d
umasoya <basilisk.0331@icloud.com>
2016-06-05 06:49:49 -0700

    Fix

--- bin/NeoBundleInstall.sh
+++ bin/NeoBundleInstall.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-function detect_distribution(){
-	distribution=`cat /etc/os-release | awk -F'["]' 'NR==1{print $2}' | awk '{print $1}'`
-}
-
-function git_install(){
-	case "$distribution" in
-		*darwin* )
-			#OSX
-			brew install git;;
-		*linux*)
-			#Ubuntu
-			apt-get install git;;
-	esac
-}
-
-
-if [ ! -e ~/.vim/bundle ];then
-	mkdir -p ~/.vim/bundle
-fi
-
-if type "git" > /dev/null 2>&1;then
-	git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-else 
-	# Processing for each OS
-	git_install
-	git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-fi
-
-vim +":NeoBundleInstall" +:q
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm -rf install.sh
