29983e18c405c30816a7836491dc936c076190cf
umasoya <basilisk.0331@icloud.com>
2016-12-20 21:23:11 +0900

    fix

--- bin/solarized.sh
+++ bin/solarized.sh
@@ -2,7 +2,7 @@
 
 # Install solarized.vim
 
-mkdir -p ~/.vim/colors
-git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors/
+mkdir -p ~/.vim/colors 2>/dev/null
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors/vim-colors-solarized/
 mv ~/.vim/colors/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
-rm -rf ~/.vim/colors/vim-colors-solarized
+rm -rf ~/.vim/colors/vim-colors-solarized/
