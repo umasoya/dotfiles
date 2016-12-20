9faacde8dc6b2a2645d082f931d565ee5b756072
umasoya <basilisk.0331@icloud.com>
2016-12-20 21:19:32 +0900

    Fixed clone command

--- bin/solarized.sh
+++ bin/solarized.sh
@@ -3,6 +3,6 @@
 # Install solarized.vim
 
 mkdir -p ~/.vim/colors
-git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors/
 mv ~/.vim/colors/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 rm -rf ~/.vim/colors/vim-colors-solarized
