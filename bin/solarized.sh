c7d81238c819c11449e4e8108dcb79fefbd6f2f1
umasoya <basilisk.0331@icloud.com>
2016-12-20 21:14:49 +0900

    Solarized.vim install script

---
+++ bin/solarized.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install solarized.vim
+
+mkdir -p ~/.vim/colors
+git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors
+mv ~/.vim/colors/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
+rm -rf ~/.vim/colors/vim-colors-solarized
