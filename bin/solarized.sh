faf8864f38e5e2e469cf82875dc1afec4e5cd605
umasoya <basilisk.0331@icloud.com>
2017-03-30 15:43:09 +0900

    Delete

--- bin/solarized.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install solarized.vim
-
-mkdir -p ~/.vim/colors 2>/dev/null
-git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/colors/vim-colors-solarized/
-mv ~/.vim/colors/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
-rm -rf ~/.vim/colors/vim-colors-solarized/ ~/.vim/colors/.git
