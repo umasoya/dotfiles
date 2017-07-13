e564bdf0d58b59e8213249a4f1b73dd700493dff
umasoya <basilisk.0331@icloud.com>
2017-07-13 10:17:17 +0900

    tmux autorun

--- .zsh/90_misc.zsh
+++ .zsh/90_misc.zsh
@@ -1,3 +1,8 @@
 # Enable autocomplete
 zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
+# Run tmux, if it is not running.
+if [ -z $TMUX ];then
+  tmux
+fi
