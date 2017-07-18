653392c8fcaf315bf790ed9ede84a7c3907dbb6b
umasoya <basilisk.0331@icloud.com>
2017-07-18 14:50:05 +0900

    Change 2line display

--- .zsh/40_prompt.zsh
+++ .zsh/40_prompt.zsh
@@ -7,48 +7,16 @@ zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
 zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
 zstyle ':vcs_info:git:*' formats "%F{green}[%b]%c%u%f"
 zstyle ':vcs_info:git:*' actionformats '[%b | %a]'
+precmd(){ vcs_info }
 
+RPROMPT="${vcs_info_msg_0_}$p_color return:[%?] %{${reset_color}%}"
 
 # Change the color according to the return value of the previous command.
 local p_color="%(?.%{${fg[cyan]}%}.%{${fg[magenta]}%})"
 
-# set position display mode
-#terminfo_down_left=$terminfo[cud1]$terminfo[cuu1]$terminfo[sc]$terminfo[cud1]
-#MODE="-- INSERT --"
-
-PROMPT_TOP=" %{$fg[cyan]%}%n@%m%{${reset_color}%}"
-PROMPT="$p_color [%~] > %{${reset_color}%}"
-
-# {{{1 RPROMPT
-add-zsh-hook precmd _update_git_status
-
-_update_git_status(){
-  LANG=C vcs_info
-  if [ -z "${vcs_info_msg_0_}" ];then
-    gitUser=''
-    RPROMPT_TOP="${gitUser}"
-    RPROMPT='${vcs_info_msg_0_}'"$p_color return:[%?] %{${reset_color}%} "
-    print -P "\n ${PROMPT_TOP}${(r:($COLUMNS-${#PROMPT_TOP}-${#RPROMPT_TOP}):: :)}$RPROMPT_TOP" 
-    return 0
-  fi
-
-  checkGitUser
-
-  print -P "\n ${PROMPT_TOP}${(r:($COLUMNS-${#PROMPT_TOP}-${#RPROMPT_TOP}):: :)}$RPROMPT_TOP" 
-}
-
-checkGitUser(){
-  local insideProject=`git rev-parse --is-inside-work-tree 2>/dev/null`
-  if [ ${insideProject} ];then
-    gitUser="`git config user.name 2>/dev/null` "
-  else
-    gitUser=''
-  fi
-
-  RPROMPT_TOP="${gitUser}"
-  RPROMPT='${vcs_info_msg_0_}'"$p_color return:[%?] %{${reset_color}%} "
-}
-# }}}
+PROMPT="
+%{$fg[cyan]%}%n@%m%{${reset_color}%}
+$p_color [%~] > %{${reset_color}%}"
 
 # {{{ Suggest prompt
 # Suggest like a google.
