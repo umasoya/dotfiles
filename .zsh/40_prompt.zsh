eda8986b967d56038ce768c1ce17001823b86bff
umasoya <basilisk.0331@icloud.com>
2017-07-18 15:59:23 +0900

    minor change

--- .zsh/40_prompt.zsh
+++ .zsh/40_prompt.zsh
@@ -1,3 +1,7 @@
+local insert="$fg[white]-- INSERT --$reset_color"
+local cmd="$fg[yellow]-- NORMAL --$reset_color"
+
+# {{{ vcs_info
 # ${vcs_info_msg_0_} : normal message
 # ${vcs_info_msg_1_} : warning message
 # ${vcs_info_msg_2_} : error message
@@ -8,17 +12,20 @@ zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
 zstyle ':vcs_info:git:*' formats "%F{green}[%b]%c%u%f"
 zstyle ':vcs_info:git:*' actionformats '[%b | %a]'
 precmd(){ vcs_info }
+# }}}
 
-RPROMPT="${vcs_info_msg_0_}$p_color return:[%?] %{${reset_color}%}"
+RPROMPT="${vcs_info_msg_0_}%{${p_color}%} return:[%?] %{${reset_color}%}"
 
 # Change the color according to the return value of the previous command.
 local p_color="%(?.%{${fg[cyan]}%}.%{${fg[magenta]}%})"
 local p_color_bold="%(?.%{${fg_bold[cyan]}%}.%{${fg_bold[magenta]}%})"
 
+# {{{ PROMPT
 PROMPT="
 %{$fg[cyan]%}%n@%m
  [%~]%{${reset_color}%} \
 ${p_color_bold}> %{${reset_color}%}"
+# }}}
 
 # {{{ Suggest prompt
 # Suggest like a google.
