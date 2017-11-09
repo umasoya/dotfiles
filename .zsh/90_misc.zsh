493bfe68c49819b38f78fc924bae0519fa57d8d6
umasoya <basilisk.0331@icloud.com>
2017-11-09 12:39:28 +0900

    Fixed tmux autorun

--- .zsh/90_misc.zsh
+++ .zsh/90_misc.zsh
@@ -3,4 +3,7 @@ zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # Run tmux, if it is not running.
-[[ -z $TMUX && ! -z $PS1 ]] > /dev/null 2>&1 && exec tmux
+#[[ -z $TMUX && ! -z $PS1 ]] > /dev/null 2>&1 && exec tmux
+if [[ $+commands[tmux] && -z $TMUX && -z $PS1 ]]; then
+        exec tmux
+fi
