7efbb2cc6fb3954789004bcd341bcff04080d18a
umasoya <basilisk.0331@icloud.com>
2017-11-23 03:41:33 +0000

    Add ignore completion pattern

--- .zsh/90_misc.zsh
+++ .zsh/90_misc.zsh
@@ -5,7 +5,7 @@ zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # Ignore completion matches pattern
-zstyle ':completion:*:*:(vi|vim):*files' ignored-patterns '(*.swp|*.tmp)'
+zstyle ':completion:*:*:(vi|vim):*files' ignored-patterns '(*.swp|*.tmp|*~|.DS_Store)'
 # Ignore completion if selected parent dir
 zstyle ':completion:*:(cd|mv|cp|gitcd):*' ignore-parents parent pwd
 # if not exists target at current candidate target at parent dir
