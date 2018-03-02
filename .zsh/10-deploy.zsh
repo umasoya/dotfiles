e32e522d42d6ae63a962e747fa33029496e0d65f
umasoya <basilisk.0331@icloud.com>
2018-03-02 09:41:27 +0000

    WIP

---
+++ .zsh/10-deploy.zsh
@@ -0,0 +1,13 @@
+local script_dir=$DOT_DIR/bin/build
+local targets=("vim" "tmux")
+
+check_installed() {
+  for i in ${@}
+  do
+    if (( !$+commands[${i}] )); then
+      . ${script_dir}/${i}
+    fi
+  done
+}
+
+check_installed ${targets}
