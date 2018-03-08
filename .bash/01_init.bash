ecca6080abf0823dcdcf1ed70da4665f6b7f8374
umasoya <basilisk.0331@icloud.com>
2018-03-08 01:58:11 +0000

    fix bash prompt

--- .bash/01_init.bash
+++ .bash/01_init.bash
@@ -14,7 +14,7 @@ if [ -f ~/.dircolors ];then
 fi
 
 # prompt
-PS1="\n\[\e[0;36m\]\h@\u\n[\w]\[\e[00m\] \$ "
+PS1="\n\[\e[0;36m\]\h@\u [\w]\n>>>\[\e[00m\] "
 
 # TAB completion
 bind TAB:menu-complete
