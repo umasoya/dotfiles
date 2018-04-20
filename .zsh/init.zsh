5601c3503330c1c9afbfc29310d6d45a0be120af
umasoya <basilisk.0331@icloud.com>
2018-04-20 07:55:54 +0000

    add pt

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,23 +1,21 @@
+os=`uname | tr '[:upper:]' '[:lower:]'`
+
 zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-syntax-highlighting", \
   defer:2, \
   from:github
 
-# zplug "b4b4r07/enhancd", use:init.sh
-# if zplug check "b4b4r07/enhancd"; then
-#         export ENHANCD_FILTER="fzf --height 50% --reverse --ansi"
-#         export ENHANCD_DOT_SHOW_FULLPATH=1
-# fi
-
 zplug "stedolan/jq", \
   as:command, \
   from:gh-r, \
+  use:"*${os}*64*"
   rename-to:"jq"
 
 zplug "direnv/direnv", \
     as:command, \
     from:gh-r, \
+    use:"*${os}*amd64*"
     rename-to:"direnv"
 
 zplug "junegunn/fzf-bin", \
@@ -25,9 +23,5 @@ zplug "junegunn/fzf-bin", \
   from:gh-r, \
   rename-to:"fzf", \
   frozen:1
-
-zplug "b4b4r07/ssh-keyreg", \
-  as:command, \
-  use:bin
 
 zplug load --verbose 1>/dev/null
