94c69247237602c35dd82692d3d77630ce491049
umasoya <basilisk.0331@icloud.com>
2019-03-05 03:03:51 +0000

    highlight config

--- .config/sh/zsh/90_misc.zsh
+++ .config/sh/zsh/90_misc.zsh
@@ -11,3 +11,14 @@
 if (( $+commands[direnv] )); then
     eval "$(direnv hook zsh)"
 fi
+
+# zsh-highlighting
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+## brackets level
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=blue,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=green,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=magenta,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=yellow,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-5]='fg=cyan,bold'
+## cursor position's brackets
+ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='standout'
