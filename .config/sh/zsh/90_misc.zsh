205a7a2803876c50365213c561813520c7d03c3a
umasoya <basilisk.0331@icloud.com>
2019-03-11 10:22:06 +0900

    clean zshrc

--- .config/sh/zsh/90_misc.zsh
+++ .config/sh/zsh/90_misc.zsh
@@ -13,7 +13,8 @@ if (( $+commands[direnv] )); then
 fi
 
 # zsh-highlighting
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+# ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main pattern cursor)
 ## brackets level
 ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=blue,bold'
 ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=green,bold'
@@ -21,4 +22,4 @@ ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=magenta,bold'
 ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=yellow,bold'
 ZSH_HIGHLIGHT_STYLES[bracket-level-5]='fg=cyan,bold'
 ## cursor position's brackets
-ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='standout'
+# ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='standout'
