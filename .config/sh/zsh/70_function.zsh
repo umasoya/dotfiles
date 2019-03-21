89978a9fbd777f4ac5b3713cad5686872784eacf
umasoya <basilisk.0331@icloud.com>
2019-03-21 12:39:24 +0900

    enhancd

--- .config/sh/zsh/70_function.zsh
+++ .config/sh/zsh/70_function.zsh
@@ -1,3 +1,18 @@
-md() {
-    pandoc $1 -s | w3m -T text/html
+# {{{ available_filter(): return available filter
+available_filter() {
+  local x candidates
+  candidates="$1:"
+  while [ -n "$candidates" ]
+  do
+    x=${candidates%%:*}
+    candidates=${candidates#*:}
+    if type "$x" > /dev/null 2>&1; then
+      echo "$x"
+      return 0
+    else
+      continue
+    fi
+  done
+  return 1
 }
+# }}}
--- .config/sh/zsh/etc/plugins.zsh
+++ .config/sh/zsh/etc/plugins.zsh
@@ -4,4 +4,10 @@ zplug "zsh-users/zsh-syntax-highlighting", \
   defer:2, \
   from:github
 
+zplug "b4b4r07/enhancd", use:init.sh
+if zplug check "b4b4r07/enhancd"; then
+  export ENHANCD_FILTER=`available_filter ${FILTER}`
+  export ENHANCD_DOT_SHOW_FULLPATH=1
+fi
+
 zplug load --verbose 1>/dev/null
