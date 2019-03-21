06f27560d178bb79c63d9a0e7f638f16a28a7afb
umasoya <basilisk.0331@icloud.com>
2019-03-21 13:16:19 +0900

    add fbr func

--- .config/sh/zsh/70_function.zsh
+++ .config/sh/zsh/70_function.zsh
@@ -24,3 +24,12 @@ repo() {
   \cd ${to}
 }
 # }}}
+
+# {{{ fbr(): checkout git branch
+fbr() {
+  local branches branch
+  branches=$(git branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+# }}}
