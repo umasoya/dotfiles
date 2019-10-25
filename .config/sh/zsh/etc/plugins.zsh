d17cc225043b6fd9c2dd66c0ca0e1ff1a6dbfd38
umasoya <basilisk.0331@icloud.com>
2019-10-25 02:50:14 +0000

    [zsh] use pure theme

--- .config/sh/zsh/etc/plugins.zsh
+++ .config/sh/zsh/etc/plugins.zsh
@@ -1,3 +1,6 @@
+zplug mafredri/zsh-async, from:github
+zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
+
 zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-syntax-highlighting", \
