a1e0cde7734ccd60c5bcef25502a19425976ff19
umasoya <basilisk.0331@icloud.com>
2020-03-10 06:29:57 +0000

    replace zplugin -> zinit

--- .config/sh/zsh/etc/zinit.zsh
+++ .config/sh/zsh/etc/zinit.zsh
@@ -13,28 +13,33 @@ fi
 
 # zsh-completions
 # -------------------------------
-zplugin ice wait lucid blockf atpull'zplugin creinstall -q .'
-zplugin light zsh-users/zsh-completions
+zinit ice wait lucid blockf atpull'zplugin creinstall -q .'
+zinit light zsh-users/zsh-completions
+
+# git completions
+# -------------------------------
+zinit ice lucid as"completion"
+zinit snippet https://github.com/git/git/blob/master/contrib/completion/git-completion.zsh
 
 # docker completions
 # -------------------------------
-zplugin ice lucid as"completion"
-zplugin snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
+zinit ice lucid as"completion"
+zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
 
 # docker-compose
 # -------------------------------
-zplugin ice lucid from"gh-r" as"program" mv"docker* -> docker-compose" bpick"${BPICK}"
-zplugin load docker/compose
+zinit ice lucid from"gh-r" as"program" mv"docker* -> docker-compose" bpick"${BPICK}"
+zinit load docker/compose
 
 # zsh-autosuggestions
 # -------------------------------
-zplugin ice wait lucid atload"_zsh_autosuggest_start"
-zplugin light zsh-users/zsh-autosuggestions
+zinit ice wait lucid atload"_zsh_autosuggest_start"
+zinit light zsh-users/zsh-autosuggestions
 
 # fast-syntax-highlighting
 # -------------------------------
-zplugin ice wait lucid atinit"zpcompinit; zpcdreplay"
-zplugin light zdharma/fast-syntax-highlighting
+zinit ice wait lucid atinit"zpcompinit; zpcdreplay"
+zinit light zdharma/fast-syntax-highlighting
 
 # pure
 # -------------------------------
@@ -44,23 +49,23 @@ zstyle :prompt:pure:git:dirty color 091
 zstyle :prompt:pure:user color blue
 zstyle :prompt:pure:host color blue
 
-zplugin ice pick"async.zsh" src"pure.zsh" atclone"npm install --global pure-prompt"
-zplugin light sindresorhus/pure
+zinit ice pick"async.zsh" src"pure.zsh" atclone"npm install --global pure-prompt"
+zinit light sindresorhus/pure
 
 
 # direnv
 # -------------------------------
-zplugin ice as"program" make'!' atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' src"zhook.zsh"
-zplugin light direnv/direnv
+zinit ice as"program" make'!' atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' src"zhook.zsh"
+zinit light direnv/direnv
 
 # fzf
 # -------------------------------
-zplugin pack for fzf
+zinit pack for fzf
 
 # jq
 # -------------------------------
-zplugin ice as"program" from"gh-r" bpick"${BPICK}" mv"jq-* -> jq"
-zplugin load stedolan/jq
+zinit ice as"program" from"gh-r" bpick"${BPICK}" mv"jq-* -> jq"
+zinit load stedolan/jq
 
 # ghq
 # -------------------------------
@@ -69,33 +74,33 @@ zplugin load stedolan/jq
 
 # hub
 # -------------------------------
-zplugin ice as"program" from"gh-r" bpick"${BPICK}" pick"*/bin/hub"
-zplugin light github/hub
-zplugin ice wait lucid as"completion" from"gh-r" id-as"hub-completion" \
+zinit ice as"program" from"gh-r" bpick"${BPICK}" pick"*/bin/hub"
+zinit light github/hub
+zinit ice wait lucid as"completion" from"gh-r" id-as"hub-completion" \
   mv"hub*/etc/hub.zsh_completion -> _hub" pick"_hub"
 
 # pt
 # -------------------------------
-zplugin ice as"program" from"gh-r" bpick"${BPICK}" pick"*/pt"
-zplugin load monochromegane/the_platinum_searcher
+zinit ice as"program" from"gh-r" bpick"${BPICK}" pick"*/pt"
+zinit load monochromegane/the_platinum_searcher
 
 # cheat
 # -------------------------------
-zplugin ice as"program" from"gh-r" bpick"${BPICK}" mv"cheat-* -> cheat"
-zplugin load cheat/cheat
+zinit ice as"program" from"gh-r" bpick"${BPICK}" mv"cheat-* -> cheat"
+zinit load cheat/cheat
 
 # enhancd
 # -------------------------------
-zplugin ice as"program" src"init.sh" \
+zinit ice as"program" src"init.sh" \
   atload'export ENHANCD_FILTER=`available_filter ${FILTER}`; export ENHANCD_DOT_SHOW_FULLPATH=1'
-zplugin load b4b4r07/enhancd
+zinit load b4b4r07/enhancd
 
 # ccat
 # -------------------------------
-zplugin ice lucid as"program" from"gh-r" bpick"${BPICK}" pick"*/ccat" atload"alias cat=ccat"
-zplugin light jingweno/ccat
+zinit ice lucid as"program" from"gh-r" bpick"${BPICK}" pick"*/ccat" atload"alias cat=ccat"
+zinit light jingweno/ccat
 
 # tmux-xpanes
 # -------------------------------
-zplugin ice lucid as"program" pick"bin/*"
-zplugin light greymd/tmux-xpanes
+zinit ice lucid as"program" pick"bin/*"
+zinit light greymd/tmux-xpanes
