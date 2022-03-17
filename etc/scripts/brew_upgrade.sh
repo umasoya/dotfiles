004bed3596d54f42dc9b283b1bd5dd848fb0a5ae
umasoya <basilisk.0331@icloud.com>
2022-03-17 19:25:48 +0900

    update brew plist

--- etc/scripts/brew_upgrade.sh
+++ etc/scripts/brew_upgrade.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 cd "${HOME}/dotfiles/brew"
 
 /opt/homebrew/bin/brew upgrade
 /opt/homebrew/bin/brew upgrade --cask
-/opt/homebrew/bin/mas upgrade
+if [[ `/opt/homebrew/bin/mas outdated` != '' ]]; then
+  echo 'update exists.'
+  /opt/homebrew/bin/mas upgrade
+fi
 
 /opt/homebrew/bin/brew bundle dump -f
+
+exit 0
