79e9b3d44504385c46b8d40782d7be5eaf916ead
umasoya <basilisk.0331@icloud.com>
2022-12-24 01:20:39 +0900

    restruct dotfiles

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+# Reset Color
+COLOR_OFF="\033[m"
+# Green
+COLOR_GREEN="\033[32m"
+
+TARGETS=(
+  config
+  git
+  misc
+  mysql
+  pt
+  tmux
+  vim
+  zsh
+)
+
+echo -e "${COLOR_GREEN}Start deploying dotfiles... ${COLOR_OFF}"
+
+# Run Deploy
+stow -vR ${TARGETS[@]}
+
+echo -e "${COLOR_GREEN}Finish deploying dotfiles‼️ ${COLOR_OFF}"
+
+# Error Handling
+on_err(){
+  error_code=$1
+  line_num=$2
+  message=$3
+  errmsg=
+  errmsg+="----------------------------\n"
+  errmsg+="Error Code : ${error_code}\n"
+  errmsg+="Message : ${message}\n"
+  errmsg+="Command Line : ${line_num}\n"
+  errmsg+="----------------------------\n"
+  echo -e ${errmsg}
+  exit ${error_code}
+}
+trap 'on_err $? $LINENO "$MESSAGE"' ERR
